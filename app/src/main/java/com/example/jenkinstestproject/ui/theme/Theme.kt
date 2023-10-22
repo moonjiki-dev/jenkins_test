001fa299a89238f3cb3a0cb275c2ab855b698e26
Moonjiki <>
2023-10-22 17:02:05 +0900

    [Commit] Commit

--- app/src/main/java/com/example/jenkinstestproject/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jenkinstestproject/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun JenkinsTestProjectTheme(
+fun  JenkinsTestProjectTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
